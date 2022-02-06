80c5dce38656220fff1260641b8820a6526db3ae
hunghoang <hth321@gmail.com>
2022-02-06 12:57:51 +0700

    fix: increase coins

--- apps/trading-bot/src/config/galaxy-top-coin.ts
+++ apps/trading-bot/src/config/galaxy-top-coin.ts
@@ -1,5 +1,5 @@
 export const LUNARCRUSH_GALAXY_TOP = 'https://api.lunarcrush.com/v2';
-export const MAX_ALT_RANK_SCORE = 100;
+export const MAX_ALT_RANK_SCORE = 200;
 export const MAX_GALAXY_SCORE = 70;
 
 export const BOT_ID = 7616300;
