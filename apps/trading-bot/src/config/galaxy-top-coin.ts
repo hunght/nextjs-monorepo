2fc05ba0eb2daee99c446620b261b660661db79c
hunghoang <hth321@gmail.com>
2022-02-22 22:44:35 +0700

    fix: don't worry about alt rank score

--- apps/trading-bot/src/config/galaxy-top-coin.ts
+++ apps/trading-bot/src/config/galaxy-top-coin.ts
@@ -1,5 +1,5 @@
 export const LUNARCRUSH_GALAXY_TOP = 'https://api.lunarcrush.com/v2';
-export const MAX_ALT_RANK_SCORE = 100;
+export const MAX_ALT_RANK_SCORE = 1000;
 export const COIN_LIMIT = 100;
 
 export const BOT_ID = 7616300;
